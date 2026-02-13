dca4b733320eba87f5cb945b56542b94bebe1c73
Tamojit Hazra <tamosrk@gmail.com>
2026-02-14 03:15:59 +0530

    same as last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Contactus } from "./components/Contactus"
 import { Footer } from "./components/Footer"
 import { Hero } from "./components/Hero"
@@ -6,7 +7,19 @@ import { Products } from "./components/Products"
 import { Testimonials } from "./components/Testimonials"
 import { Analytics } from "@vercel/analytics/react"
 
+
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hermanos" element={<Hermanos />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Hermanos() {
 
   return (
    <div className="bg-neutral-900">
@@ -20,5 +33,14 @@ function App() {
    </div>
   )
 }
+
+function Home() {
+  return (
+    <div className="bg-neutral-900 text-white p-10">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
 
 export default App
